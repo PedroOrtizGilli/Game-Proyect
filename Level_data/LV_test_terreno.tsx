8696a6cd5ec0c7b0cb053f68370a704c67ab1b6d
Pedro Ortiz Gilli <pedroortizgilli@hotmail.com>
2025-05-13 17:53:41 -0300

    Actualizacion con mapa test y organizacion nueva

---
+++ Level_data/LV_test_terreno.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terreno_de_tierra32x32" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../../Assets/terreno_de_tierra32x32.aseprite" width="132" height="131"/>
+</tileset>
