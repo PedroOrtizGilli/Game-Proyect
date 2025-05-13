b5c7d5217ea5cc4a074e3677c8b374ca9be5836d
Pedro Ortiz Gilli <pedroortizgilli@hotmail.com>
2025-05-13 15:43:57 -0300

    commmit

--- El colorista/Comandos.py
+++ El colorista/Comandos.py
@@ -8,7 +8,7 @@ def detectar_comando(jugador):
         if keys[pygame.K_d]:
             jugador.direccion.x = 1
         elif keys[pygame.K_a]:
-            jugador.direccion.x = -1    
+            jugador.direccion.x = -1   
 
         #Si no se aprieta nada la variabel que afecta al movimiento se vuelve 0 y no hay nueva pos de bliteo
         else:
